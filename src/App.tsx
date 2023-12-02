db0e7eb334d1aa2fac397781916db17b060c7f26
Francisco Jose <frano04930@gmail.com>
2023-12-01 23:51:42 -0400

    Create pagination and filter by title

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,37 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
-import P404 from './pages/404';
-import AuthLayer from './pages/Auth/AuthLayer';
-import Login from './pages/Auth/Login';
-import NewAccount from './pages/Auth/NewAccount';
-import ForgotPassword from './pages/Auth/ForgotPassword';
-import ProtectedLayer from './pages/ProtectedRoutes/ProtectedLayer';
-import Home from './pages/ProtectedRoutes/Home';
+import { Route, Routes, Navigate } from "react-router-dom";
+import P404 from "./pages/404";
+import AuthLayer from "./pages/Auth/AuthLayer";
+import Login from "./pages/Auth/Login";
+import NewAccount from "./pages/Auth/NewAccount";
+import ForgotPassword from "./pages/Auth/ForgotPassword";
+import ProtectedLayer from "./pages/ProtectedRoutes/ProtectedLayer";
+import Home from "./pages/ProtectedRoutes/Home";
 
 function App() {
-
   return (
-
-    // ROUTING 
+    // ROUTING
 
     <Routes>
-
-      <Route path='/' element={<Navigate to="/Login" />} />
-
+      <Route path="/" element={<Navigate to="/Login" />} />
 
       {/* // Routes auth  */}
 
-      <Route path='/' Component={AuthLayer}>
-
-        <Route path='Login' Component={Login} />
-        <Route path='NewAccount' Component={NewAccount} />
-        <Route path='Forgot-Password' Component={ForgotPassword} />
-
-
+      <Route path="/" Component={AuthLayer}>
+        <Route path="Login" Component={Login} />
+        <Route path="NewAccount" Component={NewAccount} />
+        <Route path="Forgot-Password" Component={ForgotPassword} />
       </Route>
 
       {/* Protecte Routes  */}
 
-      <Route path='/home' Component={ProtectedLayer}>
+      {/* <Route path='/home' Component={ProtectedLayer}> */}
+      <Route path="/home">
         <Route index Component={Home} />
-
       </Route>
 
-      <Route path='*' Component={P404} />
+      <Route path="*" Component={P404} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/ProtectedRoutes/Home.tsx
+++ src/pages/ProtectedRoutes/Home.tsx
@@ -1,6 +1,71 @@
+import {
+  Container,
+  Pagination,
+  Stack,
+  Box,
+  Paper,
+  InputBase,
+  IconButton,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+import useMoviesData from "../../hooks/useMoviesData";
+import MovieCard from "../../components/ui/MovieCard";
+
+const MAX_PAGE = 100;
 
 export default function Home() {
+  const { movies, page, setPage, setSearchTerm, searchTerm } = useMoviesData();
+  console.log("Movies", movies);
+
+  const handlePageChange = (_: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
-    <div>Home</div>
-  )
+    <Container maxWidth="lg">
+      <Paper
+        component="form"
+        sx={{
+          p: "2px 4px",
+          display: "flex",
+          alignItems: "center",
+          width: 400,
+          margin: "auto",
+        }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Filter by Title"
+          inputProps={{ "aria-label": "Filter by Title" }}
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+      <div>
+        <Stack direction="row" spacing={2} useFlexGap flexWrap="wrap">
+          {movies
+            ? movies.map((movie) => (
+                <Box sx={{ height: "20rem", width: "20rem" }}>
+                  <MovieCard movie={movie} key={movie.imdbID} />
+                </Box>
+              ))
+            : `Movie with title  ${searchTerm} not found`}
+        </Stack>
+      </div>
+      <Pagination
+        color="primary"
+        page={page}
+        onChange={handlePageChange}
+        count={MAX_PAGE}
+      />
+    </Container>
+  );
 }
